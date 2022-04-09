f301391c1ce3455ebf839d02a75a7ffb1c83df5e
Nguyễn Hùng Dũng <103114631+boynamedung@users.noreply.github.com>
2022-04-09 15:17:24 +0700

    Add files via upload

---
+++ PD for 1DOFs robot/initilize_parametersfor1DOF.m
@@ -0,0 +1,17 @@
+% File khoi dong thong so cua he robot 1 bac tu do
+% Robot & Ky thuat dieu khien hoi tiep
+% Nguyen Hung Dung MSSV:18059441 
+% Momen quan tinh
+I = 0.062;
+% Khoi luong Robot
+M = 1.1;
+% Khoi luong vat nang
+m = 0.18;
+% Chieu dai khau 
+L = 0.46;
+% Khoan cach trong tam
+Lc = 0.29;
+% He so ma sat nhot
+B = 0.10;
+% Gia toc trong truong
+g = 9.81;
